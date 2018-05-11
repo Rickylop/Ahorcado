836a73e5a6d4baa1be332fc49cc7c186d8cd242b
Ricardo <ricardolopez28@gmail.com>
2018-05-11 08:59:39 -0500

    adicion de views

--- app.rb
+++ app.rb
@@ -3,4 +3,17 @@ require './config'
 
 get '/' do
 
+	erb(:index)
+end
+
+
+post '/configurar' do
+	"Vamos a Jugar"
+	erb(:juego)
+	#session['numeroSecreto']= params['numeroSecreto']
+	#if session['numeroSecreto'].split("").uniq.size == 4  
+	#	session['mensaje']= "El numero esta configurado"
+	#else
+	#	session['mensaje']= "Numero no valido, intenta de nuevo"	
+	#end
 end
