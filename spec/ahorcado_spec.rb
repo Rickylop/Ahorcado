3e7144324d7e2333d239d7008ec3cfa6f3608262
Ricardo <ricardolopez28@gmail.com>
2018-05-11 11:30:55 -0500

    TDD

--- spec/ahorcado_spec.rb
+++ spec/ahorcado_spec.rb
@@ -1,13 +1,13 @@
 require './lib/ahorcado.rb'
 
 describe Ahorcado do
-	it "validar si la letra existe" do
-		pf = Ahorcado.new "A"
-		expect(pf.valido?).to eq true
-	end
+	#it "validar si la letra existe" do
+	#	pf = Ahorcado.new "A"
+	#	expect(pf.valido?).to eq true
+	#end
 
 	it "validar que la letra no existe" do
-		pf = Ahorcado.new "X"
-		expect(pf.valido?).to eq false
+		pf = Ahorcado.new
+		expect(pf.valido? "X").to eq false
 	end
 end
